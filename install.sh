341a9c0098c175c55f073abd04d80c12a31bd815
dellano54 <62639153+dellano54@users.noreply.github.com>
2020-06-12 05:00:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 sudo apt-get install python3 alsa-utils -y
 mkdir /opt/ip-tracker
 mv * /opt/ip-tracker
+cd ..
+rm -r Gods-eye
 cd /opt/ip-tracker/
 pip3 install -r requirements.txt
 ln tracker /bin/
