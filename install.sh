e458ec8b48548b2c3e8951239d71da98211830f6
dellano samuel fernandez <62639153+dellano54@users.noreply.github.com>
2025-06-06 22:40:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ mkdir /opt/gods-eye
 mv * /opt/gods-eye
 
 rm -r "../Gods-eye"
-cd /opt/gods-eye/
-pip3 install -r requirements.txt
+pip3 install -r /opt/gods-eye/requirements.txt
 ln gods-eye /bin/
 chmod +x /usr/bin/gods-eye
 echo "\nNOW YOU CAN ENTER gods-eye IN ANY TERMINAL TO OPEN gods-eye"
